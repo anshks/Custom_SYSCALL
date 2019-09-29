d32fb46188e99a9a1623af979608a6d4213965b4
anshks <41546616+anshks@users.noreply.github.com>
2019-09-29 22:22:54 +0530

    Create tester.c

---
+++ tester.c
@@ -0,0 +1,20 @@
+#include<unistd.h>
+#include<sys/syscall.h>
+#include<stdio.h>
+#include<linux/kernel.h>
+#include<errno.h>
+long main() {
+	printf("Enter PID\n");
+	pid_t pid;
+  	scanf("%d", &pid);
+  	printf("Enter filename");
+  	char *fname;
+  	scanf("%s", fname);
+  	long out = syscall(355, pid, fname);
+  	if (out != 0) {
+    	printf("Error\n");
+   	}
+   	perror("err");
+   	printf("\nerrno: %d\n", errno);
+   	return 0;	
+}
